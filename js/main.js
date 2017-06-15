1b356aefeb3578213ed493cf91b80f2877ce4658
Graciela Cesareo Cruz <gracielacesareocruz@Gracielas-MacBook-Pro.local>
2017-06-15 17:00:53 -0500

    intento de filtro, aun no funciona

--- js/main.js
+++ js/main.js
@@ -1,26 +1,31 @@
-var api = {
-    url: 'https://examen-laboratoria-sprint-5.herokuapp.com/topics'
-}
+var api = 'https://examen-laboratoria-sprint-5.herokuapp.com/topics';
 
 var $listaTemas = $("#temas");
 
 var cargarPagina = function () {
     leerTemas();
     $("#add-form").submit(agregarTema);
+    $("#search-form").submit(filtrarTema);
 }
 
 var leerTemas = function () {
-    $.getJSON(api.url, function(temas) {
+    $.getJSON(api, function(temas) {
         temas.forEach(crearTema);
     });
 }
 
-var crearTema = function (tema) {
-    var tema = tema.content;
-    var autor = tema.author_name;
-    var respuestas = tema.responses_count;
+var plantilla = '<tr>' +
+                   '<td>__tema__</td>' +
+                   '<td>__autor__</td>' +
+                   ' <td>__respuestas__ </td>'+
+                '</tr>';
 
-    var $tr = $("<tr />");
+var crearTema = function (response) {
+    var tema = response.content;
+    var autor = response.author_name;
+    var respuestas = response.responses_count;
+
+    /*var $tr = $("<tr />");
     var $temaTd = $("<td />");
     $temaTd.text(tema);
 
@@ -34,21 +39,39 @@ var crearTema = function (tema) {
     $tr.append($autorTd);
     $tr.append($respuestas);
 
-    $listaTemas.append($tr)
+    $listaTemas.append($tr)*/
+    
+    var plantillaNueva = plantilla.replace("__tema__", tema).replace("__autor__",autor).replace("__respuestas__",respuestas);
+    
+    $listaTemas.append(plantillaNueva);
 };
+
 
 var agregarTema = function (e) {
   e.preventDefault();
   //alert("probando");
   var nuevoAutor = $("#nombre-autor").val();
   var nuevoTema = $("#nuevo-tema").val();
-  $.post(api.url, {
+  $.post(api, {
       author_name : nuevoAutor,
       content: nuevoTema
   }, function (tema) {
     crearTema(tema);
     $("#myModal").modal("hide");
   });
+};
+
+var filtrarTema = function (e) {
+    e.preventDefault(); 
+    var busqueda = $("#search").val().toLowerCase();
+    
+    $.getJSON(api,function(tema){
+        var tema = tema.filter(function(response){
+            return
+            (response.content).toLowerCase().indexOf(busqueda) >=0;
+        });
+        crearTema(tema);
+    });       
 };
 
 $(document).ready(cargarPagina);
