fe46fcd8ce8262a347f23aa83d9fa81e833a168a
Graciela Cesareo Cruz <gracielacesareocruz@Gracielas-MacBook-Pro.local>
2017-06-15 11:40:19 -0500

    funcionalidad para agregar contenido a la lista

--- js/main.js
+++ js/main.js
@@ -6,6 +6,7 @@ var $listaTemas = $("#temas");
 
 var cargarPagina = function () {
     leerTemas();
+    $("#add-form").submit(agregarTema);
 }
 
 var leerTemas = function () {
@@ -17,18 +18,37 @@ var leerTemas = function () {
 var crearTema = function (tema) {
     var tema = tema.content;
     var autor = tema.author_name;
-    
+    var respuestas = tema.responses_count;
+
     var $tr = $("<tr />");
     var $temaTd = $("<td />");
     $temaTd.text(tema);
-    
+
     var $autorTd = $("<td />");
     $autorTd.text(autor);
-    
+
+    var $respuestas = $("<td />");
+    $respuestas.text(respuestas);
+
     $tr.append($temaTd);
     $tr.append($autorTd);
-    
+    $tr.append($respuestas);
+
     $listaTemas.append($tr)
+};
+
+var agregarTema = function (e) {
+  e.preventDefault();
+  //alert("probando");
+  var nuevoAutor = $("#nombre-autor").val();
+  var nuevoTema = $("#nuevo-tema").val();
+  $.post(api.url, {
+      author_name : nuevoAutor,
+      content: nuevoTema
+  }, function (tema) {
+    crearTema(tema);
+    $("#myModal").modal("hide");
+  });
 };
 
 $(document).ready(cargarPagina);
